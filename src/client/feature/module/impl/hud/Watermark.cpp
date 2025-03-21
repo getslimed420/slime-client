184da19a6dc674d169939b0f1c2416872c8d5976
getslimed420 <bryceicepp@gmail.com>
2025-03-20 18:29:25 -0700

    Update Watermark.cpp

--- src/client/feature/module/impl/hud/Watermark.cpp
+++ src/client/feature/module/impl/hud/Watermark.cpp
@@ -1,2 +1,8 @@
 #include "pch.h"
 #include "Watermark.h"
+#include "client/Latite.h"
+
+Watermark::Watermark() : TextModule("Watermark", LocalizeString::get("client.textmodule.watermark.name"),
+                                      LocalizeString::get("client.textmodule.watermark.desc"), HUD) {
+    this->prefix = TextValue(L"SLIME");
+}
