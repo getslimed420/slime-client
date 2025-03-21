9e534b1877ba56a47bb5a6142907b840cb56e74e
getslimed420 <bryceicepp@gmail.com>
2025-03-20 18:25:37 -0700

    Update Watermark.h

--- src/client/feature/module/impl/hud/Watermark.h
+++ src/client/feature/module/impl/hud/Watermark.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "../../TextModule.h"
 
-class Watermark : public Module {
+class Watermark : public TextModule {
 public:
+	Watermark();
+
+	std::wstringstream text(bool isDefault, bool inEditor) override;
+};
